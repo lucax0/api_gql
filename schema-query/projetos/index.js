04b1ddc7e3694f889a927b969b7b4caaae259ef7
Matchbox <lucas.uesato@matchboxbrasil.com>
2021-02-04 19:44:18 -0300

    Criando queryes / resolvers e scalar

--- schema-query/projetos/index.js
+++ schema-query/projetos/index.js
@@ -1,1 +1,39 @@
-console.log('test222e')
+const { ApolloServer, gql} = require('apollo-server')
+
+const typeDefs = gql`
+    #Criar scalar "tipos"
+    scalar Date
+
+    #Ponto de entrada da api
+    type Query {
+        ola: String!        
+        adeus: String!
+        horaCerta: Date
+    }
+
+
+`
+
+const resolvers = {
+
+    Query: {
+        ola() {
+            return 'string de ola!'
+        },
+        adeus() {
+            return 'string de adeus!'
+        },
+        horaCerta() {
+            return new Date
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen(4000).then(({ url }) =>{
+    console.log(`Executando em ${url}`)
+})
